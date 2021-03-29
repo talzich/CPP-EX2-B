6ee530bfcbbb35384ba6185ccaff309e53ad8362
Tal <talzhtc@gmail.com>
2021-03-29 22:08:32 +0300

    Added some more documentation

--- Board.cpp
+++ Board.cpp
@@ -1,4 +1,6 @@
+//
 // AUTHOURS: Tal Zichlinsky
+//
 // This file will be used to build a message board class, as described in this assignments README document (https://github.com/cpp-exercises/messageboard-a)
 
 #include "Board.hpp"
@@ -19,11 +21,14 @@ const int MIN_SIZE = 1;
 // Margins of messageboard
 const int MARGIN = 5;
 
+// Constants for getting new board sizes after board resize
 const int ROWS = 0;
 const int COLS = 1;
 
+// Default char
 const char EMPTY_SPACE = '_';
 
+// This method gets location of requested action (read or post) and resizes the board if location is out of current board size
 vector<unsigned int> resize_board(unsigned int row, unsigned int col, Direction dir, unsigned int len, vector< vector<char> > &b){
 
 
@@ -43,9 +48,8 @@ vector<unsigned int> resize_board(unsigned int row, unsigned int col, Direction 
         size_t old_size = board_rows;
         board_rows += diff + MARGIN;
 
-        // Resizing our board row number and adding margins
+        // Resizing our board row number and adding margins. For loop fills each row with an empty space charecter
         b.resize(board_rows);
-
         for(size_t i = old_size; i < board_rows; i++){
             b[i].resize(board_cols, EMPTY_SPACE);
         }
@@ -54,7 +58,6 @@ vector<unsigned int> resize_board(unsigned int row, unsigned int col, Direction 
     // If message will not fit in this board horizontally
     if (col_end_pos >= board_cols)
     {
-
         // How bigger do we need our board to be
         size_t diff = static_cast<size_t>(col_end_pos - board_cols);
 
@@ -67,16 +70,14 @@ vector<unsigned int> resize_board(unsigned int row, unsigned int col, Direction 
         }
     }
 
+    // New size for this board
     vector<unsigned int> new_size {board_rows, board_cols};
     return new_size;
-
 }
 
 // Default constructor
 Board::Board()
 {
-
-    // No size is given as parameter so initializing to default size
     rows = DEFAULT_SIZE;
     cols = DEFAULT_SIZE;
 
@@ -93,7 +94,6 @@ Board::Board()
 // Two parameter constructor
 Board::Board(unsigned int rows, unsigned int cols)
 {
-
     if (rows < MIN_SIZE)
     {
         throw invalid_argument("Invalid input: " + to_string(rows));
@@ -106,12 +106,20 @@ Board::Board(unsigned int rows, unsigned int cols)
 
     this->rows = rows;
     this->cols = cols;
+
+    // Initializing vector with number of rows
+    board.resize(rows);
+
+    // Initializing each row of the vector with number of columns and the empty space char
+    for (unsigned int i = 0; i < this->rows; i++)
+    {
+        board[i].resize(cols, EMPTY_SPACE);
+    }
 }
 
 // Single parameter constructor
 Board::Board(unsigned int size)
 {
-
     if (size < MIN_SIZE)
     {
         throw invalid_argument("Invalid input: " + to_string(rows));
@@ -119,6 +127,15 @@ Board::Board(unsigned int size)
 
     this->rows = size;
     this->cols = size;
+
+    // Initializing vector with number of rows
+    board.resize(rows);
+
+    // Initializing each row of the vector with number of columns and the empty space char
+    for (unsigned int i = 0; i < this->rows; i++)
+    {
+        board[i].resize(cols, EMPTY_SPACE);
+    }
 }
 
 // Destructor
@@ -128,7 +145,6 @@ Board::~Board() {}
 // appropriate location and diraction in this message board
 int Board::post(unsigned int row, unsigned int col, Direction dir, string msg)
 {
-
     // Length of message
     unsigned int len = msg.size();
 
@@ -144,9 +160,7 @@ int Board::post(unsigned int row, unsigned int col, Direction dir, string msg)
         for(unsigned int i = 0; i < len; i++){
             this->board[row][col+i] = msg[i];
         }
-    }
-
-    else{
+    }else{
         // Posting message
         for(unsigned int i = 0; i < len; i++){
             this->board[row+i][col] = msg[i];
@@ -159,7 +173,6 @@ int Board::post(unsigned int row, unsigned int col, Direction dir, string msg)
 // appropriate location and diraction in this message board
 string Board::read(unsigned int row, unsigned int col, Direction dir, unsigned int len)
 {
-
     // Resizing the board, if necessary
     vector<unsigned int> new_size = resize_board(row, col, dir, len, this->board);
 
@@ -167,28 +180,26 @@ string Board::read(unsigned int row, unsigned int col, Direction dir, unsigned i
     this->rows = new_size[ROWS];
     this->cols = new_size[COLS];
 
+    // The string to return
     string msg;
 
-    
+    // Retrieving string from board
     if(dir == Direction::Horizontal){
         for(unsigned int i = 0; i < len; i++){
             msg += this->board[row][col+i];
         }
-        return msg;
-    }
-
-    
-    for(unsigned int i = 0; i < len; i++){
+    }else{
+        for(unsigned int i = 0; i < len; i++){
         msg += this->board[row+i][col];
+        }
     }
     return msg;
-    
 }
 
-// This message will print out the entire message board
+// This method will print out the entire message board
 void Board::show()
-{
-
+{   
+    cout << endl;
     for (size_t i = 0; i < this->rows; i++)
     {
         cout << i << '\t';
