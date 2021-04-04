e58d57f4a7ffef73dd23534bc2b4cc9247354bcb
Tal <talzhtc@gmail.com>
2021-04-04 09:59:11 +0300

    Centered main

--- Prog.cpp
+++ Prog.cpp
@@ -13,7 +13,7 @@ using namespace ariel;
 using namespace std;
 
 const Direction HORI = Direction::Horizontal;
-
+const Direction VERT = Direction::Vertical;
 
 int main(void){
 
@@ -22,65 +22,66 @@ int main(void){
     unsigned int col;
     string msg = "";
 
-    b.post(3,5, HORI, "Hello");
-    b.post(4,5, HORI, "Harel!");
+    b.post(3,3, HORI, "Hello");
+    b.post(4,3, HORI, "Harel!");
     b.show();
 
     system("read");
-    b.post(7,5, HORI, "Oh, that's not it, right?");
-    b.post(8,5, HORI, "I'll fix it than!");
+    b.post(7,3, HORI, "Oh, that's not right, is it?");
+    b.post(8,3, HORI, "I'll fix it than!");
     b.show();
 
     system("read");
     
-    b.post(4,5, HORI, "Erel!");
-    b.post(7,5, HORI, "____There, Done!_________");
-    b.post(8,5, HORI, "_________________");
+    b.post(4,3, HORI, "Erel!");
+    b.post(7,3, HORI, "____There, Done!______________");
+    b.post(8,3, HORI, "_________________");
     b.show();
 
-    b.post(8, 5, HORI, "Now look at that, my size changes dynamically!");
+    b.post(8, 3, HORI, "Now look at that, my size changes dynamically!");
     b.show();
     system("read");
 
-    b.post(8, 5, HORI, "Now look at that, my size changes dynamically!!");
+    b.post(8, 3, HORI, "Now look at that, my size changes dynamically!!");
     b.show();
     system("read");
     
-    b.post(8, 5, HORI, "Now look at that, my size changes dynamically!!!");
+    b.post(8, 3, HORI, "Now look at that, my size changes dynamically!!!");
     b.show();
     system("read");
 
-    b.post(8, 5, HORI, "Now look at that, my size changes dynamically!!!!!!!!!!!!!!!!!!!!");
+    b.post(8, 3, HORI, "Now look at that, my size changes dynamically!!!!!!!!!!!!!!!!!!!!");
     b.show();
     system("read");
 
-    b.post(9, 5, HORI, "I can also grow vertically! WATCH THAT!!!");
+    b.post(9, 3, HORI, "I can also grow vertically! WATCH THAT!!!");
     b.show();
     system("read");
 
     //Printing arrow
     for(int i = 1; i < 9; i++){
-        b.post(i, 13, HORI, "######");
+        b.post(i, 11, HORI, "######");
     }
-    b.post(9, 5, HORI,  "########################");
-    b.post(10, 5, HORI, " ######################");   
-    b.post(11, 5, HORI, "  ####################");   
-    b.post(12, 5, HORI, "   ##################");  
-    b.post(13, 5, HORI, "    ################");  
-    b.post(14, 5, HORI, "     ##############");  
-    b.post(15, 5, HORI, "      ############");
-    b.post(16, 5, HORI, "       #########");
-    b.post(17, 5, HORI, "        #######");
-    b.post(18, 5, HORI, "         #####");
-    b.post(19, 5, HORI, "          ###");
-    b.post(20, 5, HORI, "           #");
+    b.post(9, 3, HORI,  "########################");
+    b.post(10, 3, HORI, " ######################");   
+    b.post(11, 3, HORI, "  ####################");   
+    b.post(12, 3, HORI, "   ##################");  
+    b.post(13, 3, HORI, "    ################");  
+    b.post(14, 3, HORI, "     ##############");  
+    b.post(15, 3, HORI, "      ############");
+    b.post(16, 3, HORI, "       #########");
+    b.post(17, 3, HORI, "        #######");
+    b.post(18, 3, HORI, "         #####");
+    b.post(19, 3, HORI, "          ###");
+    b.post(20, 3, HORI, "           #");
     b.show();
 
     system("read");
-    b.post(22, 5, HORI, "See that? Previous messages gets overwritten by new ones!");
-    b.post(23, 5, HORI, "What would you like to write, and where?");
+    b.post(22, 3, HORI, "See that? Previous messages gets overwritten by new ones!");
+    b.post(23, 3, HORI, "What would you like to write, and where?");
     b.show();
 
+    char dir = 0;
     while(1){
         cout << "Enter row number" << endl;
         cin >> row;
@@ -88,13 +89,28 @@ int main(void){
         cout << "Enter column number" << endl;
         cin >> col;
 
+        while(dir != 'v' && dir != 'h'){
+        cout << "Enter direction v or h" <<endl;
+        cin >> dir;
+        }
+
         cin.clear();
         fflush(stdin);
         cout << "Enter message" << endl;
         getline(cin, msg);
 
+        switch (dir)
+        {
+        case 'v':
+            b.post(row, col, VERT, msg);
+            break;
         
-        b.post(row, col, HORI, msg);
+        case 'h':
+            b.post(row, col, HORI, msg);
+        default:
+            perror("Wrong direction has been entered");
+            exit(1);
+        }
         b.show();
     }
     return 0;
